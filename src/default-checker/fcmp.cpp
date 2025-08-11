7f8b5eb56c8c12bb8af6749c80af6f9ec7af345c
tobiichi3227 <cz1346219@gmail.com>
2025-08-11 16:02:02 +0800

    testlib diff-strict weird

--- src/default-checker/fcmp.cpp
+++ src/default-checker/fcmp.cpp
@@ -1,33 +1,27 @@
-#include "testlib.h"
-#include <string>
+#include <fstream>
+#include <cstring>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    setName("compare files as sequence of lines");
-    registerTestlibCmd(argc, argv);
-
-    std::string strAnswer;
-
-    int n = 0;
-    while (!ans.eof()) {
-        std::string j = ans.readString();
-
-        if (j.empty() && ans.eof())
-            break;
-
-        strAnswer = j;
-        std::string p = ouf.readString();
-
-        n++;
-
-        if (j != p)
-            quitf(_wa, "%d%s lines differ - expected: '%s', found: '%s'", n, englishEnding(n).c_str(),
-                  compress(j).c_str(), compress(p).c_str());
+bool StrictCompare(std::ifstream& f_ans, std::ifstream& f_usr) {
+  constexpr size_t kBufSize = 65536;
+  static char buf1[kBufSize], buf2[kBufSize];
+  size_t pos = 0;
+  while (f_ans) {
+    f_ans.read(buf1, kBufSize);
+    f_usr.read(buf2, kBufSize);
+    if (f_ans.eof() != f_usr.eof() || f_ans.gcount() != f_usr.gcount()) {
+      return false;
     }
+    if (memcmp(buf1, buf2, f_ans.gcount()) != 0) {
+      return false;
+    }
+    pos += f_usr.gcount();
+  }
+  return true;
+}
 
-    if (n == 1)
-        quitf(_ok, "single line: '%s'", compress(strAnswer).c_str());
-
-    quitf(_ok, "%d lines", n);
+int main(int argc, char *argv[]) {
+    std::ifstream test_out(argv[2]), user_ans(argv[3]);
+    return !StrictCompare(test_out, user_ans);
 }
